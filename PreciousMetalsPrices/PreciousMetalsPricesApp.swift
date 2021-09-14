3cf49488c878ca2dc9cba68f902448097eb50819
Andrey <a.balakhmet@gmal.com>
2021-09-14 11:47:50 +0600

    Initial Commit

---
+++ PreciousMetalsPrices/PreciousMetalsPricesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreciousMetalsPricesApp.swift
+//  PreciousMetalsPrices
+//
+//  Created by Admin on 14.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct PreciousMetalsPricesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
